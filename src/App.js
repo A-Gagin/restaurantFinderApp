ec930bf95b82b497eefd33e4b2c73726bee51fc7
A-Gagin <afg4pr@virginia.edu>
2021-05-23 01:05:42 -0400

    Made api call, filtered for open locations only

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+//import GetRestaurants from "./GetRestaurants"; -- uses axios... but for some reason eats up a bunch of cpu? not sure why. Makes machine inoperable
+
+const API_KEY = process.env.REACT_APP_api_key;
 
 function App() {
+
+  // MAKE THIS INTO A SEPARATE COMPONENT
+  const [restaurants, setRestaurants] = useState(null);
+  console.log("restaurants API call", restaurants); // testing
+
+  const getRestaurants = () => {
+    const url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json");
+    url.searchParams.append("key", API_KEY);
+    url.searchParams.append("location", "38.0293,-78.4767"); // for now this only looks at Charlottesville. Use Geocoding API later.
+    url.searchParams.append("radius", 10 * 1609.34); // Max distance from current location. '* 1609.34' converts meters to miles.
+    url.searchParams.append("type", "restaurant"); // Allow for user filtering later.
+    url.searchParams.append("opennow", ""); // Only display locations that are open.
+
+
+    fetch(url)
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((obj) => {
+        if (obj.status === "OK") {
+          setRestaurants(obj.results);
+        } else {
+          setRestaurants(null);
+        }
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getRestaurants}>
+        Get Restaurants
+      </button>
+      {/* <GetRestaurants setRestaurants={setRestaurants}/> */}
+
     </div>
   );
 }
